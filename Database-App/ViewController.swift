18fac23db31addd43a1321a802fba4fb842570f7
Ionut <nicolae.ionut9711@gmail>
2019-11-18 13:59:24 +0200

    Initial Commit

---
+++ Database-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Database-App
+//
+//  Created by Nicolae Ionut on 11/18/19.
+//  Copyright © 2019 Nicolae Ionut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
